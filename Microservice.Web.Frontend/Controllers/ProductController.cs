74c14478821d7935603772d070b6c2696eecb7dc
SayyehBan <sdvp1991david@gmail.com>
2024-04-02 18:26:40 +0330

    Add project files.

---
+++ Microservice.Web.Frontend/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice.Web.Frontend.Controllers;
+
+public class ProductController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
