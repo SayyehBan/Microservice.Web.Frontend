fde44d75d566c89b7c80b0350ddb83aa8f3f6575
SayyehBan <sdvp1991david@gmail.com>
2024-04-02 22:16:42 +0330

    نمایش محصولات

--- Microservice.Web.Frontend/Controllers/ProductController.cs
+++ Microservice.Web.Frontend/Controllers/ProductController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microservice.Web.Frontend.Services.ProductServices;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Microservice.Web.Frontend.Controllers;
 
 public class ProductController : Controller
 {
+    private readonly IProductService productService;
+
+    public ProductController(IProductService productService)
+    {
+        this.productService = productService;
+    }
     public IActionResult Index()
     {
-        return View();
+        var products = productService.GetAllProduct();
+        return View(products);
     }
+    public IActionResult Details(Guid Id)
+    {
+        var product = productService.GetProduct(Id);
+        return View(product);
+    }
+    
 }
