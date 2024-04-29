e014c57fa255bceaad92352b3fe84cde769f418b
SayyehBan <sdvp1991david@gmail.com>
2024-04-29 14:24:24 +0330

    ارسال پیام Eventual Consistency بروزرسانی از طرف سرویس محصول

--- Microservice.Web.Frontend/Models/LinkService/LinkServices.cs
+++ Microservice.Web.Frontend/Models/LinkService/LinkServices.cs
@@ -1,11 +1,10 @@
-﻿namespace Microservice.Web.Frontend.Models.LinkService
+﻿namespace Microservice.Web.Frontend.Models.LinkService;
+
+public static class LinkServices
 {
-    public static class LinkServices
-    {
-        public static string BasketService = "https://localhost:44386/";
-        public static string DiscountService = "https://localhost:7091/";
-        public static string OrderService = "https://localhost:44376/";
-        public static string ProductService = "https://localhost:44304/";
-        public static string PaymentService = "https://localhost:44390/";
-    }
+    public static string BasketService = "https://localhost:44386/";
+    public static string DiscountService = "https://localhost:7091/";
+    public static string OrderService = "https://localhost:44376/";
+    public static string ProductService = "https://localhost:44304/";
+    public static string PaymentService = "https://localhost:44390/";
 }
